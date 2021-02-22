b800894f9748485737980fe94e2a2282275479c0
s-t-a-n <31728406+s-t-a-n@users.noreply.github.com>
2021-02-22 13:35:30 +0100

    Added strjoin_reuse : same as strjoin, frees input strings

--- inc/libft.h
+++ inc/libft.h
@@ -141,6 +141,7 @@ char				*ft_strndup(const char *s1, size_t len);
 char				*ft_strsub(char const *s, size_t start, size_t len);
 char				*ft_substr(char const *s, size_t start, size_t len);
 char				*ft_strjoin(char const *s1, char const *s2);
+char				*ft_strjoin_noreuse(char const *s1, char const *s2);
 char				*ft_strtrim(char const *s1, char const *set);
 char				**ft_strsplit(char const *s, char c);
 char				**ft_split(char const *s, char c);
