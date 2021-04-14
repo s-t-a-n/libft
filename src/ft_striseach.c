868db56db7d3e0b7bbac510060213d12214c3617
Stan Verschuuren <31728406+s-t-a-n@users.noreply.github.com>
2021-04-14 12:01:45 +0200

    Ft striseach (#18)
    * Makefile, libft.h, ft_striseach.c: added ft_striseach()

---
+++ src/ft_striseach.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   ft_striseach.c                                     :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: sverschu <sverschu@student.codam.n>          +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2020/06/19 22:01:50 by sverschu      #+#    #+#                 */
+/*   Updated: 2020/06/19 22:01:52 by sverschu      ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_bool	ft_striseach(const char *str, int (*f)(int c))
+{
+	while (*str)
+	{
+		if (!f(*str))
+			return (FALSE);
+		str++;
+	}
+	return (TRUE);
+}
